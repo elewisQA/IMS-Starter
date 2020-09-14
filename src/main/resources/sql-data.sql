35d00f753bb8991ea7c84a27327c765d94078984
Elizabeth Lewis <elewis@academytrainee.com>
2020-09-14 10:49:49 +0100

    Added some data to the sql-data script.

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,4 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('JORDAN', 'HARRISON');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('BENDER', 'RODRIGUEZ');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('LEELA', 'TURANGA');
+INSERT INTO `ims`.`items` (`name`, `description`) VALUES ('SLURM', 'Amazing Slurm drink!');
