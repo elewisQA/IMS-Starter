ca4c9eb2c3bcc40accc5af4fbdf6153b8464044d
Elizabeth Lewis <elewis@academytrainee.com>
2020-09-14 11:12:28 +0100

    Added equals() method.

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -58,7 +58,28 @@ public class Item {
 	
 	@Override
 	public boolean equals(Object obj) {
-		//TODO fill this out - matching structure of customer.equals() method
-		return false;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass()) 
+			return false;
+		Item other = (Item) obj;
+		if (getName() == null) {
+			if (other.getName() != null)
+				return false
+		} else if (!getName().contentEquals(other.getName()))
+			return false;
+		if (id == null) {
+			if (other.id != null) 
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		return true;
 	}
 }
