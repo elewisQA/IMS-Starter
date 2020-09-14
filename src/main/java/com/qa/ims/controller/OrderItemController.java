8e87b1030717acdb0a51e0c51c33938d9ec988fb
Elizabeth Lewis <elewis@academytrainee.com>
2020-09-14 14:59:33 +0100

    Added all basic Order-Item classes.

---
+++ src/main/java/com/qa/ims/controller/OrderItemController.java
@@ -0,0 +1,63 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderItemDAO;
+import com.qa.ims.persistence.domain.OrderItem;
+import com.qa.ims.utils.Utils;
+
+public class OrderItemController implements CrudController<OrderItem> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private OrderItemDAO orderItemDAO;
+	private Utils utils;
+	private Long oid;
+	
+	public OrderItemController(OrderItemDAO orderItemDAO, Utils utils) {
+		super();
+		this.orderItemDAO = orderItemDAO;
+		this.utils = utils;
+	}
+
+	@Override 
+	public List<OrderItem> readAll() {
+		List<OrderItem> orderItems = orderItemDAO.readAll();
+		for (OrderItem orderItem : orderItems) {
+			LOGGER.info(orderItem.toString());
+		}
+		return orderItems;
+	}
+	
+	@Override
+	public OrderItem create() {
+		LOGGER.info("Please enter an Item id");
+		Long iid = utils.getLong();
+		LOGGER.info("Please enter a quantity");
+		Long qty = utils.getLong();
+		OrderItem orderItem = orderItemDAO.create(new OrderItem(oid, iid, qty));
+		return orderItem;
+	}
+	
+	@Override
+	public OrderItem update() {
+		LOGGER.info("Please enter an item-id");
+		Long iid = utils.getLong();
+		LOGGER.info("Please enter a quantity");
+		Long qty = utils.getLong();
+		OrderItem order = orderItemDAO.update(new OrderItem(oid, iid, qty));
+		LOGGER.info("Order-Item updated");
+		return order;
+	}
+	
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the item-id of the order you would like to delete");
+		Long iid = utils.getLong();
+		LOGGER.info("Order-Item deleted.");
+		return orderItemDAO.delete(iid);
+	}
+}
---
+++ src/main/java/com/qa/ims/persistence/dao/OrderItemDAO.java
@@ -0,0 +1,114 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.OrderItem;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderItemDAO implements Dao<OrderItem> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	@Override
+	public OrderItem modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long oid = resultSet.getLong("oid");
+		Long iid = resultSet.getLong("iid");
+		Long qty = resultSet.getLong("qty");
+		return new OrderItem(oid, iid, qty);
+	}
+
+	@Override
+	public List<OrderItem> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM order_items");) {
+			List<OrderItem> orderItems = new ArrayList<>();
+			while (resultSet.next()) {
+				orderItems.add(modelFromResultSet(resultSet));
+			}
+			return orderItems;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public OrderItem readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM order_items ORDER BY oid DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	@Override
+	public OrderItem create(OrderItem orderItem) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("INSERT INTO order_items(iid, qty) VALUES(" 
+				+ orderItem.getIid() + "," + orderItem.getQty() + ")");
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public OrderItem readOrderItem(Long oid) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM order_items WHERE oid = " + oid);) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public OrderItem update(OrderItem orderItem) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("UPDATE order_items SET iid = "
+				+ orderItem.getIid() + ", qty = " + orderItem.getQty());
+			return readOrderItem(orderItem.getOid());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public int delete(long oid) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			return statement.executeUpdate("delete from order_items where iid = " + oid);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
+	
+	
+}
