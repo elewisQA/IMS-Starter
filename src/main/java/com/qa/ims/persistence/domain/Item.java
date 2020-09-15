4c0f2c7e5957bbe6cf46d18113aa6eb02aecdfdc
Elizabeth Lewis <elewis@academytrainee.com>
2020-09-15 15:51:37 +0100

    Changed cost from a Long to a Double in line with the SQL field type

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -2,17 +2,18 @@ package com.qa.ims.persistence.domain;
 
 public class Item {
 	
-	private Long id, cost;
+	private Long id;
+	private Double cost;
 	private String name;
 	private String description;
 	
-	public Item(String name, String description, Long cost) {
+	public Item(String name, String description, Double cost) {
 		this.setName(name);
 		this.setDescription(description);
 		this.setCost(cost);
 	}
 	
-	public Item(Long id, String name, String description, Long cost) {
+	public Item(Long id, String name, String description, Double cost) {
 		this.setId(id);
 		this.setName(name);
 		this.setDescription(description);
@@ -43,11 +44,11 @@ public class Item {
 		this.description = description;
 	}
 	
-	public Long getCost() {
+	public Double getCost() {
 		return cost;
 	}
 	
-	public void setCost(Long cost) {
+	public void setCost(Double cost) {
 		this.cost = cost;
 	}
 	
@@ -63,6 +64,7 @@ public class Item {
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((cost == null) ? 0 : cost.hashCode());
 		return result;
 	}
 	
@@ -84,13 +86,17 @@ public class Item {
 		if (id == null) {
 			if (other.id != null) 
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!id.equals(other.getId()))
 			return false;
 		if (description == null) {
 			if (other.description != null)
 				return false;
-		} else if (!description.equals(other.description))
+		} else if (!description.equals(other.getDescription()))
 			return false;
+		if (cost == null) {
+			if (other.cost != null)
+				return false;
+		} else if (!cost.equals(other.getCost()))
 		return true;
 	}
 }
