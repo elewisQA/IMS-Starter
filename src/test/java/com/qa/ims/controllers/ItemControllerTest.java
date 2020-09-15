34d0a3d3b820d6026216eea95085d52c3f4d9e82
Elizabeth Lewis <elewis@academytrainee.com>
2020-09-15 16:14:02 +0100

    Added basic of ItemController Test. Further test methods required.

---
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -0,0 +1,48 @@
+package com.qa.ims.controllers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.ims.controller.ItemController;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemControllerTest {
+	
+	@Mock
+	private Utils utils;
+	
+	@Mock
+	private ItemDAO dao;
+	
+	@InjectMocks
+	private ItemController controller;
+	
+	@Test
+	public void testCreate() {
+		final String NAME = "SLURM", DESCRIPTION = "SLURM COLA!";
+		final Double COST = 2.99;
+		final Item created = new Item(NAME, DESCRIPTION, COST);
+		
+		Mockito.when(utils.getString()).thenReturn(NAME, DESCRIPTION);
+		Mockito.when(utils.getDouble()).thenReturn(COST);
+		Mockito.when(dao.create(created)).thenReturn(created);
+		
+		assertEquals(created, controller.create());
+		
+		Mockito.verify(utils, Mockito.times(2)).toString();
+		Mockito.verify(dao, Mockito.times(1)).create(created);
+	}
+
+}
